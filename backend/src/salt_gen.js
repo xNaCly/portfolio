e0edccd8515f5ca007fb66568dd74624c934cc92
xnacly <matteogropp@gmail.com>
2021-04-02 11:13:18 +0200

    d

--- backend/src/salt_gen.js
+++ backend/src/salt_gen.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
-const { salt } = require("./config.json");
 // bcrypt.genSalt(Math.floor(Math.random() * 1000) + 1).then(console.log);
-bcrypt.hash("" /* pw */, 12).then(console.log);
-bcrypt.compare("" /*plaintext pw */, "" /*hash*/).then(console.log);
+// bcrypt.hash("" /* pw */, 12).then(console.log);
+bcrypt
+	.compare(
+		"khpYIhFMYDisjlsrpiZhuReH" /*plaintext pw */,
+		"$2b$12$SujEGFnw49NwW2Sv0PIXXOeWXwTe4jHmtZqCC.Oi8iQk6PO/BH9R." /*hash*/
+	)
+	.then(console.log);
