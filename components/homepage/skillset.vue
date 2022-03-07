cd66add86f878af01af40a773c8d30efb2dd5da8
Matteo Gropp <matteogropp@protonmail.com>
2022-03-07 11:44:57 +0100

    feat(home): added skillset display

---
+++ components/homepage/skillset.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="skillset-wrapper">
+		<h2 class="navigator_header">Skillset:</h2>
+		<div class="skillset-container skillsets-languages" data-aos="fade-left">
+			<SkillsetItem
+				v-for="skill in skills"
+				:title="skill.title"
+				:key="skill.date"
+				:date="skill.date"
+				:level="skill.level"
+			/>
+		</div>
+	</div>
+</template>
+<script>
+import SkillsetItem from "../util/skillset_item.vue";
+import { skills as skills_ } from "../../content/skills.json";
+skills_.sort((a, b) => {
+	// if (a.level < b.level) return -1;
+	// if (a.level > b.level) return 1;
+	// return 0;
+	return new Date(a.date) - new Date(b.date);
+});
+
+export default {
+	name: "Skillset",
+	data() {
+		return {
+			skills: skills_,
+		};
+	},
+	components: {
+		SkillsetItem,
+	},
+};
+</script>
---
+++ components/util/skillset_item.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="skillset" data-aos="fade-left">
+		<div class="skillset-title">
+			{{ capitalise(title) }}
+			<span class="skillset-experience">{{ formatTime(date) }}</span>
+		</div>
+		<div class="badge skillset-badge" :class="`skillset-badge-${level}`">{{ capitalise(level) }}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "SkillsetItem",
+	methods: {
+		capitalise: function (string) {
+			string = string?.toLowerCase();
+			let fc = string?.charAt(0).toUpperCase();
+			return fc + string?.slice(1);
+		},
+		formatTime: function (timestamp) {
+			let string = "";
+			let days = (Date.now() - new Date(timestamp)) / (1000 * 3600 * 24);
+			console.log(new Date(timestamp), days);
+
+			if (days > 360) {
+				string = `${(days / 360).toFixed(1)} years`;
+			} else if (days > 30 && days < 360) {
+				string = `${Math.round(days / 30)} months`;
+			} else {
+				string = `${Math.round(days)} days`;
+			}
+
+			return string;
+		},
+	},
+	props: ["title", "level", "date"],
+};
+</script>
