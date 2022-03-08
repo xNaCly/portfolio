72446c69971ab227bb944f37fbdc662a7003850d
Matteo Gropp <matteogropp@protonmail.com>
2022-03-08 12:19:06 +0100

    feat(blog_entry): added indentation to subheadings

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -51,7 +51,11 @@
 					<h3>Table of Contents:</h3>
 					<div class="toc-link-container">
 						<ul>
-							<li v-for="link of article.toc" :key="link.id">
+							<li
+								v-for="link of article.toc"
+								:key="link.id"
+								:class="{ toc2: link.depth === 2, toc3: link.depth === 3 }"
+							>
 								<NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
 							</li>
 						</ul>
